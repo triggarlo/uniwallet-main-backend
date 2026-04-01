1e8e6f70a5be7e7650115523c8116c208f5e9872
anthony audu <you@gmail.com>
2026-04-01 06:48:06 +0100

    Fix pre-save hook

--- src/models/User.js
+++ src/models/User.js
@@ -26,11 +26,10 @@ const UserSchema = new mongoose.Schema({
 }, { timestamps: true });
 
 // Hash password before save
-UserSchema.pre('save', async function(next) {
-  if (!this.isModified('password')) return next();
+UserSchema.pre('save', async function() {
+  if (!this.isModified('password')) return;
   const salt = await bcrypt.genSalt(12);
   this.password = await bcrypt.hash(this.password, salt);
-  next();
 });
 
 // Compare passwords
